95f86e47d4c75fb9cbccd690d2d661d25cf938b8
Calvin LeVally <clevally@astutesolutions.com>
2020-09-14 15:13:04 -0400

    removing some stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,38 +19,8 @@
 </svelte:head>
 
 <main>
-
-	<div class="card">
-		<div class="card-image">
-		  <figure class="image is-4by3">
-			<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-		  </figure>
-		</div>
-		<div class="card-content">
-		  <div class="media">
-			<div class="media-left">
-			  <figure class="image is-48x48">
-				<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-			  </figure>
-			</div>
-			<div class="media-content">
-			  <p class="title is-4">John Smith</p>
-			  <p class="subtitle is-6">@johnsmith</p>
-			</div>
-		  </div>
-	  
-		  <div class="content">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-			Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-			<a href="#">#css</a> <a href="#">#responsive</a>
-			<br>
-			<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-		  </div>
-		</div>
-	  </div>
-
 	{#await fetchLeagueData}
-		<p>...waiting</p>
+	<progress class="progress is-large is-primary" max="100">60%</progress>
 	{:then data}
 		{@debug data}
 		<p>Ready</p>
